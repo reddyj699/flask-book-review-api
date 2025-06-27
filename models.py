314b5f1dd0fcc0c19c48db1f0a6a0fb5e9b28732
Jaswanth reddy <106111871+reddyj699@users.noreply.github.com>
2025-06-27 08:15:42 +0530

    Add files via upload

---
+++ models.py
@@ -0,0 +1,16 @@
+# models.py
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    reviews = db.relationship('Review', backref='book')
+
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    book_id = db.Column(db.Integer, db.ForeignKey('book.id'), nullable=False)
